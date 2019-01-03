003ea9c05e4ef4ed4dd67439d6ace1efc74ee44b
Helloman892 <1565516+Helloman892@users.noreply.github.com>
2019-01-03 22:05:12 +0000

    init

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/ly_interpreter"
---
+++ src/ly_interpreter.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `LyInterpreter`
+module LyInterpreter
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
