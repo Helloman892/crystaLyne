9e7ce9edbc4beefa762edd8eda8eec82f76fb7e1
Helloman892 <1565516+Helloman892@users.noreply.github.com>
2019-01-21 14:59:40 +0000

    breaking out of loops doesn't work properly; pre-commit before I mess with loops in general

--- spec/ly_interpreter_spec.cr
+++ spec/ly_interpreter_spec.cr
@@ -1,9 +1,16 @@
-require "./spec_helper"
+require "stdio"
+require "spec2"
+require "interpreter.cr"
 
-describe LyInterpreter do
-  # TODO: Write tests
+Spec2.describe Ly do
+  it "can do loops" do
+    (flags = Hash(Symbol, Bool | Float64 | Time::Span).new false)[:start] = Time::Span.new(nanoseconds: 0)
+    expect(Stdio.capture do |io|
+      described_class.execute "(2706)(410)[sfl%]pu", flags
+      io.out.gets
+    end.not_nil!.chomp).to eq "82"
+  end
 
-  it "works" do
-    false.should eq(true)
+  it "can check primality of numbers outside the range of 64-bit ints" do
   end
 end
